38166f9081bc89ad83f29439381d1ee21155c9a8
ioanachi <ioana_chichi@outlook.com>
2017-09-08 18:17:21 +0300

    Hover not

--- firstTheme/js/script.js
+++ firstTheme/js/script.js
@@ -1,1 +1,27 @@
-console.log('ntraaaaaaaaaaaaa');
+jQuery(document).ready(function() {
+  var imgx = $('img.imagine');
+  var buttony = $('.button');
+  var imagini = ['img/1.jpg', 'img/2.jpg'];
+  console.log();
+  // imgx.attr('src', imagini[0]);
+  var apasat = false;
+  buttony.click(function() {
+    if (apasat) {
+      imgx.attr('src', imagini[1]);
+      apasat = false;
+    } else {
+      imgx.attr('src', imagini[0]);
+      apasat = true;
+
+    }
+
+  });
+
+
+});
+
+$('.container a.menu').hover(.colo.fadeIn(500));
+//     $('.container a.menu').(100 );
+//     $('.container a.menu').fadeIn( 500 );
+// });
+console.log("martini");
