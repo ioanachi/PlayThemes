bea2184a263a795567f6a12a032a291b82ba4850
Ciprian Turcu <turcuciprian1@gmail.com>
2017-09-12 23:23:06 +0300

    added jquery to the project

--- themePr/src/js/script.js
+++ themePr/src/js/script.js
@@ -1,1 +1,6 @@
-console.log('asd');
+// creating the $ variable from jquery
+var $ = require("jquery");
+// when document finished loading
+$(document).ready(function() {
+  // jquery code on load here
+});
