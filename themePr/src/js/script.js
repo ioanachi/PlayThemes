963c97dc2e252dbe854960a8d3bdf43206b611b3
ioanachi <ioana_chichi@outlook.com>
2017-10-07 12:46:22 +0300

    nothing to commit

--- themePr/src/js/script.js
+++ themePr/src/js/script.js
@@ -3,4 +3,21 @@ var $ = require("jquery");
 // when document finished loading
 $(document).ready(function() {
   // jquery code on load here
+  // 
+  // $('body').click(function(){
+  //   $(this).css('background','red');
+  // });
+  //
+  //
+  // var b = $('body');
+  // function ce_sa_faca(){
+  //   $(this).css('background','red');
+  // }
+  // var ce_sa_faca = function(){
+  //   $(this).css('background','red');
+  // }
+  // var arr = [];
+  //
+  // b.click(ce_sa_faca);
+
 });
